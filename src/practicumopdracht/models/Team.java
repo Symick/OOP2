c2eaa074b8ab3ec80f4077efd2d8e4d04e459e1f
Julian Kruithof <julian.kruithof@hva.nl>
2023-02-19 20:19:41 +0100

    added getters and setters to Team model

--- src/practicumopdracht/models/Team.java
+++ src/practicumopdracht/models/Team.java
@@ -5,4 +5,43 @@ public class Team {
     private int firstEntryYear;
     private boolean active;
     private int teamChampionships;
+
+    public Team(String name, int firstEntryYear, boolean active, int teamChampionships) {
+        this.name = name;
+        this.firstEntryYear = firstEntryYear;
+        this.active = active;
+        this.teamChampionships = teamChampionships;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFirstEntryYear() {
+        return firstEntryYear;
+    }
+
+    public void setFirstEntryYear(int firstEntryYear) {
+        this.firstEntryYear = firstEntryYear;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public int getTeamChampionships() {
+        return teamChampionships;
+    }
+
+    public void setTeamChampionships(int teamChampionships) {
+        this.teamChampionships = teamChampionships;
+    }
 }
